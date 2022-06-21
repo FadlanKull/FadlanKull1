78110ebc33fd2572a11ded13bae105f919369eb1
FadlanKull <107873064+FadlanKull@users.noreply.github.com>
2022-06-21 21:35:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,30 @@ By : ${chalk.bold('xDast#8745')} - Credit : @dkmpostor & @Eskey & @Fadlan
 (consola.info(` [ ${moment().format('HH:mm:ss')} ] ${chalk.magenta(`User : ${username}`)} ${chalk.red(`Server : ${country}`)} ${chalk.blue(`Trophy : ${trophy}`)} ${chalk.green(`Crown : ${crown}`)}`));
       await sleep(2000);
 
+${chalk.green(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+${chalk.green(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+${chalk.green(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+await sleep(0);
+console.info(chalk.white(`\rTime : [ ${moment().format('HH:mm:ss')} ] ${chalk.red(`!`)}${chalk.white(`User : ${username}`)} | ${chalk.red(`!`)}${chalk.white(`Trophy : ${trophy}`)} | ${chalk.red(`!`)}${chalk.white(`Crown : ${crown}`)}
+${chalk.red(` Status : Succes✓`)}\n`));
+${chalk.green(` Status : Succes✓`)}\n`));
+await sleep(0);
+
     } else if (result == 'BANNED') {
       console.log(consola.error(`Akun Anda ke band? Gw Party 🤘🤘😎😎 !!`));
      break
